c282a8fdba72e6fb1eeac94bdb4eb2a63d411526
Darcey Grundy <grundy.darcey@gmail.com>
2021-09-01 23:34:20 -0400

    part 13

--- validation.js
+++ validation.js
@@ -1,3 +1,21 @@
+const inputs = document.querySelectorAll('input');
+
 const patterns = {
   telephone: /^\d[11]$/
+};
+
+function validate(field, regex) {
+  //console.log(regex.test(field.value));
+  if (regex.test(field.value)) {
+    field.className = 'valid';
+  } else {
+    field.className = 'invalid';
+  }
 }
+
+inputs.forEach((input) => {
+  input.addEventListener('keyup', (e) => {
+    //console.log(e.target.attributes.name.value);
+    validate(e.target, patterns[e.target.attributes.name.value]);
+  });
+});
